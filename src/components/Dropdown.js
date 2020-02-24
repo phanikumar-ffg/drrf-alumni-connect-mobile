0d610328bd9725000ec70b0877b51a60decf4435
saisruthinandyala <30804662+saisruthinandyala@users.noreply.github.com>
2020-02-24 23:13:14 +0530

    help screen and drop-down component

---
+++ src/components/Dropdown.js
@@ -0,0 +1,17 @@
+import React, { memo } from 'react';
+import RNPickerSelect from 'react-native-picker-select';
+
+export const Dropdown = () => {
+    return (
+        <RNPickerSelect
+            onValueChange={(value) => console.log(value)}
+            items={[
+                { label: 'Football', value: 'football' },
+                { label: 'Baseball', value: 'baseball' },
+                { label: 'Hockey', value: 'hockey' },
+            ]}
+        />
+    );
+};
+
+export default memo(Dropdown);
