35131f9f6109d8ae67a021a9561e8d5f8b3c8e7e
phanikumar-ffg <59959469+phanikumar-ffg@users.noreply.github.com>
2020-06-14 22:35:34 +0530

    Update AdminAddContentScreen.js

--- src/screens/AdminAddContentScreen.js
+++ src/screens/AdminAddContentScreen.js
@@ -20,114 +20,158 @@ import DatePicker from 'react-native-datepicker';
 import RNPickerSelect from 'react-native-picker-select';
 import _ from 'lodash';
 import { connect } from 'react-redux';
-import {authContentChange, addContent} from '../actions'
+import { authContentChange, addContent } from '../actions';
 
-import {
-
-} from '../core/utils';
+import {} from '../core/utils';
 
 import PropTypes from 'prop-types';
 
-const contentTypeOptions = [ { label: 'Video', value: 'Video' },
-                             { label: 'Document', value: 'Document' },
-                             { label: 'Website', value: 'Website'}
-                           ];
+const contentTypeOptions = [
+  { label: 'Video', value: 'Video' },
+  { label: 'Document', value: 'Document' },
+  { label: 'Website', value: 'Website' },
+];
 
 class AdminAddContentScreen extends React.Component {
-    componentWillReceiveProps(nextProps) {
-        if (!_.isEmpty(nextProps.user)) {
-          this.props.navigation.navigate('Dashboard', nextProps.user);
-        }
-      }
-
-    submitContent() {
-          console.debug('submitContent');
-          const { contentURL, contentType, contentDesc, assessmentURL } = this.props;
-          /*if(url=="" || description=="" || assessURL=="" || contentType=="")
-          $("#validation").setValue("Please enter all fields")*/
-          this.props.addContent({ contentURL,contentType, contentDesc, assessmentURL });
+  componentWillReceiveProps(nextProps) {
+    if (!_.isEmpty(nextProps.user)) {
+      this.props.navigation.navigate('Dashboard', nextProps.user);
     }
+  }
 
-    showButton() {
-        if (this.props.loading) {
-          return (
-            <View>
-              <ActivityIndicator size="small" />
-            </View>
-          );
-        } else {
-          return (
-            <Button mode="contained" onPress={this.submitContent.bind(this)}>
-              SAVE
-            </Button>
-          );
-        }
-      }
+  submitContent() {
+    console.log('submitContent');
+    const { contentURL, contentType, contentDesc, assessmentURL } = this.props;
+    console.log(contentURL);
+    this.props.addContent({
+      contentURL,
+      contentType,
+      contentDesc,
+      assessmentURL,
+    });
+  }
 
-    showError() {
-       if (this.props.error) {
-          return <Paragraph>{this.props.error}</Paragraph>;
-       }
+  showButton() {
+    if (this.props.loading) {
+      return (
+        <View>
+          <ActivityIndicator size="small" />
+        </View>
+      );
+    } else {
+      return (
+        <Button mode="contained" onPress={this.submitContent.bind(this)}>
+          SAVE
+        </Button>
+      );
     }
-     _onSave() {
-        return (
-          //   <Alert>Saved Successfully</Alert>
-          <View style={styles.container}>
-            <Paragraph>Saved Successfully</Paragraph>
+  }
+
+  showError() {
+    if (this.props.error) {
+      return <Paragraph>{this.props.error}</Paragraph>;
+    }
+  }
+  _onSave() {
+    return (
+      //   <Alert>Saved Successfully</Alert>
+      <View style={styles.container}>
+        <Paragraph>Saved Successfully</Paragraph>
+      </View>
+    );
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <Background>
+          <BackButton
+            goBack={() => this.props.navigation.navigate('HomeScreen')}
+          />
+          <Logo />
+          <Header>Dr. Reddy's Foundation</Header>
+          <Header>Add New Content</Header>
+          <View style={styles.formStyle}>
+            <form>
+              <TextInput
+                label="Video URL"
+                returnKeyType="next"
+                value={this.props.contentURL}
+                required
+                onChangeText={value =>
+                  this.props.authContentChange({
+                    field: 'contentURL',
+                    value: value,
+                  })
+                }
+                autoCapitalize="none"
+              />
+              <RNPickerSelect
+                useNativeAndroidPickerStyle="false"
+                placeholder={{ label: 'Content Type', value: null }}
+                value={this.props.contentType}
+                style={pickerStyle}
+                onValueChange={value => console.log(value)}
+                items={contentTypeOptions}
+              />
+              <TextInput
+                label="Description"
+                returnKeyType="next"
+                value={this.props.contentDesc}
+                required
+                onChangeText={value =>
+                  this.props.authContentChange({
+                    field: 'contentDesc',
+                    value: value,
+                  })
+                }
+                autoCapitalize="none"
+              />
+              <TextInput
+                label="Assessment URL"
+                returnKeyType="next"
+                value={this.props.assessmentURL}
+                required
+                onChangeText={value =>
+                  this.props.authContentChange({
+                    field: 'assessmentURL',
+                    value: value,
+                  })
+                }
+                autoCapitalize="none"
+              />
+            </form>
           </View>
-        );
-      };
 
-    render(){
-        return (
-            <ScrollView>
-              <Background>
-                <BackButton goBack={() => this.props.navigation.navigate('HomeScreen') } />
-                <Logo />
-                <Header>Dr. Reddy's Foundation</Header>
-                <Header>Add New Content</Header>
-                <View style={styles.formStyle}>
-                  <form>
-                     <TextInput  label="Video URL" returnKeyType="next" value={this.props.contentURL} required
-                              onChangeText={value => this.props.authContentChange({ field: 'url', value: value }) }
-                              autoCapitalize="none" />
-                     <RNPickerSelect useNativeAndroidPickerStyle="false"
-                                     placeholder={{ label: 'Content Type',value: null,}}
-                                     value={this.props.contentType} style={pickerStyle} onValueChange={value => console.log(value)} items={contentTypeOptions} />
-                     <TextInput  label="Description" returnKeyType="next" value={this.props.contentDesc} required
-                                 onChangeText={value => this.props.authContentChange({ field: 'description', value: value }) }
-                                 autoCapitalize="none" />
-                     <TextInput  label="Assessment URL" returnKeyType="next" value={this.props.assessmentURL} required
-                                  onChangeText={value => this.props.authContentChange({ field: 'assessURL', value: value }) }
-                                  autoCapitalize="none" />
-                  </form>
-                </View>
-
-                  <Button mode="contained" onPress={this.submitContent.bind(this)} >
-                            SAVE
-                          </Button>
-                  <Button mode="contained" onPress={() => this.props.navigation.navigate('AdminAddContentScreen') }>
-                            RESET
-                   </Button>
-
-              </Background>
-            </ScrollView>
-          );
-    }
+          <Button mode="contained" onPress={this.submitContent.bind(this)}>
+            SAVE
+          </Button>
+          <Button
+            mode="contained"
+            onPress={() =>
+              this.props.navigation.navigate('AdminAddContentScreen')
+            }
+          >
+            RESET
+          </Button>
+        </Background>
+      </ScrollView>
+    );
+  }
 }
 const pickerStyle = StyleSheet.create({
-    inputAndroid: {
-      backgroundColor: theme.colors.surface,
-      paddingTop: 16,
-      paddingBottom: 16,
-      paddingRight: 4,
-      borderRadius: 4,
-      width: '100%',
-      marginVertical: 12,
-      color: '#999',
-      borderColor: '#808080',
-    },
-  });
+  inputAndroid: {
+    backgroundColor: theme.colors.surface,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingRight: 4,
+    borderRadius: 4,
+    width: '100%',
+    marginVertical: 12,
+    color: '#999',
+    borderColor: '#808080',
+  },
+});
 
 const styles = StyleSheet.create({
   label: {
@@ -160,11 +204,11 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = state => {
   return {
-    contentURL: state.auth.contentURL,
-    contentType: state.auth.contentType,
-    contentDesc: state.auth.contentDesc,
-    assessmentURL: state.auth.assessmentURL,
-    loading: state.auth.loading,
+    contentURL: state.addContent.contentURL,
+    contentType: state.addContent.contentType,
+    contentDesc: state.addContent.contentDesc,
+    assessmentURL: state.addContent.assessmentURL,
+    loading: state.addContent.loading,
   };
 };
 
