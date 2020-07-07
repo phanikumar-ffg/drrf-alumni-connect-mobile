1336fc6de5b440a76bb0f7c6e971cfe2a52c9897
arjun-goyal <arj.goyal207@gmail.com>
2020-07-08 01:52:49 +0530

    final changes

--- src/screens/JobSearch.js
+++ src/screens/JobSearch.js
@@ -52,12 +52,12 @@ const JobSearch = (props) => {
             </View>
             <View style = {{flex: 1, flexDirection: 'row',  marginLeft: '10%', marginBottom: '10%'}}>
                 <Icon name='place' color="#0b2652" size={25} style = {{flex:1}}/>   
-                <Text style={styles.popupText}> {jobSelected.city}</Text>
+                <Text style={styles.popupText}> {jobSelected.cityName}</Text>
             </View>
             <Text style={{flex:1, width: "100%"}}><hr /></Text>
             <View style = {{flex:1, flexDirection: 'row'}}>
-                <PaperButton mode = "contained" labelStyle = {styles.text} style={styles.noButton} onPress = {() => {setdialogVisibility(false)}}>No</PaperButton>
-                <PaperButton mode = "contained"  labelStyle = {styles.text} style={styles.yesButton} onPress = {() => sendJobRequest()} loading = {yesButtonLoading}>Yes</PaperButton>
+                <PaperButton mode = "contained" labelStyle = {styles.text} style={styles.noButton} onClick = {() => {setdialogVisibility(false)}}>No</PaperButton>
+                <PaperButton mode = "contained"  labelStyle = {styles.text} style={styles.yesButton} onClick = {() => sendJobRequest()} loading = {yesButtonLoading}>Yes</PaperButton>
             </View>
         </View>
     )
@@ -75,31 +75,34 @@ const JobSearch = (props) => {
 
     //Touchable opacity view component
     const TouchableOpacityView = (
-        <TouchableOpacity style = {styles.touchOpacity} onPress ={ ()=>{setdialogVisibility(false)}}></TouchableOpacity>
+        <TouchableOpacity disabled = {true} style = {styles.touchOpacity} onPress ={ ()=>{setdialogVisibility(false)}}></TouchableOpacity>
     )
 
 
     //Function runs once only when Job Search screen is rendered to get job data
     useEffect(() => {
-        fetch(config.baseurl+'/api/v1/jobs/'+props.user.studentId)
+        fetch(config.baseurl+'/api/v1/jobs/'+props.user.aspirantId)
         .then(response => {
             if (response.status != 200){
                 setLoaderVisibility(false)
-                setAlertParameters({message: "Unable to fetch jobs, Internal Server Error", backgroundColor: '#e6c8c8', icon: 'error', iconColor: '#611010'})
+                setAlertParameters({message: "Unable to fetch jobs", backgroundColor: '#e6c8c8', icon: 'error', iconColor: '#611010'})
                 setAlertVisibility(true)
+                return "Server Error"
             }
             else{
                 return response.json()
         }})
-        .then(jobs => {
+        .then(res => {
             setLoaderVisibility(false)
-            if (Array.isArray(jobs) && jobs.length) {
-            setData({value: jobs});
-            setDataBackup({value: jobs});
+            if (Array.isArray(res) && res.length) {
+            setData({value: res});
+            setDataBackup({value: res});
             }
             else {
-                setAlertParameters({message: "No jobs found", backgroundColor: '#f0eabd', icon: 'warning', iconColor: '#665c10'})
-                setAlertVisibility(true)
+                if (res!="Server Error"){
+                    setAlertParameters({message: "No jobs found", backgroundColor: '#f0eabd', icon: 'warning', iconColor: '#665c10'})
+                    setAlertVisibility(true)
+                }
             }
         })
         .catch(err => {
@@ -136,7 +139,7 @@ const JobSearch = (props) => {
         searchText = text.trim().toLowerCase();
         if (!searchText == "") {
             var filteredData = dataBackup.value.filter(l => {
-                return l.city.trim().toLowerCase().startsWith( searchText );
+                return l.cityName.trim().toLowerCase().startsWith( searchText );
             });
             setData({value: filteredData})
         }
@@ -173,8 +176,8 @@ const JobSearch = (props) => {
         fetch(config.baseurl+'/api/v1/jobrequest', {
             method: 'POST',
             body: JSON.stringify({
-                studentId: props.user.studentId,
-                studentEmail: props.user.email,
+                studentId: props.user.aspirantId,
+                studentEmail: props.user.emailId,
                 studentName: props.user.firstName+' '+props.user.lastName,
                 jobId: jobSelected.jobId,
                 jobRole: jobSelected.designation,
@@ -189,11 +192,11 @@ const JobSearch = (props) => {
             setButtonLoading(false)
             setdialogVisibility(false)
             if (response.status == 200){
-                setAlertParameters({message: "Your request was successfully sent", backgroundColor: '#b6e0bc', icon: 'check-circle', iconColor: '#146110'})
+                setAlertParameters({message: "Request successfully sent", backgroundColor: '#b6e0bc', icon: 'check-circle', iconColor: '#146110'})
                 removeAppliedJobEntry();
             }
             else {
-                setAlertParameters({message: "Request not sent, Internal Server Error", backgroundColor: '#e6c8c8', icon: 'error', iconColor: '#611010'})
+                setAlertParameters({message: "Request not sent", backgroundColor: '#e6c8c8', icon: 'error', iconColor: '#611010'})
             }
             setAlertVisibility(true)
             setTimeout(()=>{
@@ -234,7 +237,7 @@ const JobSearch = (props) => {
                         Company: {j.companyName}
                     </Text>
                     <Text style={{marginBottom: 8}}>
-                        Location: {j.city}
+                        Location: {j.cityName}
                     </Text>
                     <Text style={{marginBottom: 8}}>
                         Description: {j.jobDescription}
@@ -261,16 +264,12 @@ const styles = StyleSheet.create({
   },
   yesButton: {
     flex: 0.5,
-    marginRight: "20%",
-    marginLeft: "2%",
-    marginVertical: '3%',
+    margin: '3%',
     backgroundColor: '#3f9e3a'
   },
   noButton: {
     flex: 0.5, 
-    marginLeft: "20%",
-    marginRight: "2%",
-    marginVertical: '3%',
+     margin: '3%',
     backgroundColor: '#e35b5b'
   },
 popupContainer: {
