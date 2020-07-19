c54371baa91090f28f8c6b89212e309e38d34203
deepthivemulaffg <deepthi.vemula@tsgforce.com>
2020-07-19 19:52:43 +0530

    adjusting alignments of user and admin home screens

--- src/screens/UserHomeScreen.js
+++ src/screens/UserHomeScreen.js
@@ -10,36 +10,32 @@ import Header from '../components/Header';
 export default class UserHomeScreen extends Component {
   render() {
     return (
-     <View>
-      <Background>
-          <Logo />
-          <Header>Better Skills Better Jobs</Header>
-          <View style = {{justifyContent: "flex-start",flexWrap: "wrap", marginLeft: 0}} >
-          <View style ={{flex:1, flexDirection: 'row'}} >
-            <Card containerStyle={{width:'40%'}}   style = {styles.icon}>
-               <Icon name='person'  size={100} onPress={() => this.props.navigation.navigate('ProfileScreen')} />
-               <Text style = {styles.text} > Profile Maintenance </Text>
-            </Card>
-            <Card containerStyle={{width:'40%'}}  style = {styles.icon}  >
-               <Icon name='search'  size={100}  onPress={() => this.props.navigation.navigate('JobSearch')}/>
-               <Text style = {styles.text} > Job Search </Text>
-            </Card>
-          </View>
-          <View style ={{flex:1, flexDirection: 'row'}} >
-             <Card containerStyle={{width:'40%'}}   style = {styles.icon} >
-                 <Icon name='help'  size={100}  onPress={() => this.props.navigation.navigate('HelpScreen')}/>
-                 <Text style = {styles.text} > DRF Help </Text>
-             </Card>
-             <Card containerStyle={{width:'40%'}} style = {styles.icon} >
-                  <Icon name='image'  size={100} onPress={() => this.props.navigation.navigate('HomePage')} />
-                  <Text style = {styles.text} > New Content </Text>
-             </Card>
-          </View>
-          </View>
-
-       </Background>
-      </View >
-
+    <ScrollView>
+         <Background>
+              <Logo />
+              <Header>Better Skills Better Jobs</Header>
+              <View style ={{flex:1, flexDirection: 'row'}} >
+                <Card containerStyle={styles.container}   style = {styles.icon}>
+                   <Icon name='person'  size={100} onPress={() => this.props.navigation.navigate('ProfileScreen')} />
+                   <Text style = {styles.text} > Profile Maintenance </Text>
+                </Card>
+                <Card containerStyle={styles.container}  style = {styles.icon}  >
+                   <Icon name='search'  size={100}  onPress={() => this.props.navigation.navigate('JobSearch')}/>
+                   <Text style = {styles.text} > Job Search </Text>
+                </Card>
+              </View>
+              <View style ={{flex:1, flexDirection: 'row'}} >
+                 <Card containerStyle={styles.container}   style = {styles.icon} >
+                     <Icon name='help'  size={100}  onPress={() => this.props.navigation.navigate('HelpScreen')}/>
+                     <Text style = {styles.text} > DRF Help </Text>
+                 </Card>
+                 <Card containerStyle={styles.container} style = {styles.icon} >
+                      <Icon name='image'  size={100} onPress={() => this.props.navigation.navigate('HomePage')} />
+                      <Text style = {styles.text} > New Content </Text>
+                 </Card>
+              </View>
+              </Background>
+          </ScrollView >
     );
   }
 }
@@ -48,7 +44,8 @@ const styles = StyleSheet.create({
      justifyContent: "flex-start",
      flexDirection: "row",
      flexWrap: "wrap",
-     marginTop: 30
+     marginTop: 30,
+     width:'40%',
   },
   text: {
     alignItems: 'center',
@@ -58,12 +55,11 @@ const styles = StyleSheet.create({
   icon: {
      alignItems: 'center',
      justifyContent: 'center',
-     marginBottom: 20,
+     marginBottom: 0,
      marginRight: "50%",
      marginVertical: '3%',
      flex : 0.5,
      backgroundColor: '#eee',
-     height : '100%',
      width : 100
   }
   });
